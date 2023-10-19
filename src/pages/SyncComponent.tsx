8c6841d4a4f425dc1ee0978f6424677bcc34d1e2
Robert-d-s <110900093+Robert-d-s@users.noreply.github.com>
2023-10-19 13:48:32 +0200

    ..

---
+++ src/pages/SyncComponent.tsx
@@ -0,0 +1,40 @@
+import React, { useEffect } from "react";
+
+const SyncComponent: React.FC = () => {
+  useEffect(() => {
+    const syncTeams = async () => {
+      // Retrieve the token from local storage
+      const token = localStorage.getItem("token");
+
+      try {
+        const response = await fetch(
+          "http://localhost:8080/team-synchronize/teams",
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+              // Add Authorization header if a token is present
+              ...(token ? { Authorization: `Bearer ${token}` } : {}),
+            },
+          }
+        );
+
+        const data = await response.json();
+        console.log("Sync Response:", data);
+      } catch (error) {
+        console.error("Error during sync:", error);
+      }
+    };
+
+    syncTeams();
+  }, []); // The empty dependency array ensures this useEffect runs once when the component mounts.
+
+  return (
+    <div>
+      <h1>Team Synchronization</h1>
+      <p>Check the console for synchronization results.</p>
+    </div>
+  );
+};
+
+export default SyncComponent;
