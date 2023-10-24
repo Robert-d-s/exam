95b55eba7b61200b5ef20b0fbeeaf30a2e0fbe0f
Robert-d-s <110900093+Robert-d-s@users.noreply.github.com>
2023-10-24 14:31:47 +0200

    form checks

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -6,9 +6,40 @@ const Signup: React.FC = () => {
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const router = useRouter();
+  const [errors, setErrors] = useState<{ email?: string; password?: string }>(
+    {}
+  );
+  const passwordRegex =
+    /^(?=.*[A-Z])(?=.*[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]).{8,}$/;
+
+  const validateForm = () => {
+    let valid = true;
+    const newErrors: { email?: string; password?: string } = {};
+
+    // Check if email is valid
+    const emailRegex = /\S+@\S+\.\S+/;
+    if (!email || !emailRegex.test(email)) {
+      newErrors.email = "Please provide a valid email address.";
+      valid = false;
+    }
+
+    // Check if password is valid
+    if (!password || !passwordRegex.test(password)) {
+      newErrors.password =
+        "Password must be at least 8 characters long, have at least one uppercase letter, and one special character.";
+      valid = false;
+    }
+
+    setErrors(newErrors);
+    return valid;
+  };
 
   const handleSignup = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+
+    if (!validateForm()) {
+      return;
+    }
 
     try {
       const response = await fetch("http://localhost:8080/auth/signup", {
@@ -21,6 +52,14 @@ const Signup: React.FC = () => {
           password,
         }),
       });
+
+      if (response.status === 409) {
+        setErrors({
+          ...errors,
+          email: "Email already exists.",
+        });
+        return;
+      }
 
       if (response.ok) {
         console.log("Signup successful. Redirecting to login...");
@@ -47,6 +86,9 @@ const Signup: React.FC = () => {
             placeholder="Email"
             className="mb-4 w-full p-3 border border-gray-300 rounded-md"
           />
+          {errors.email && (
+            <p className="text-red-500 text-sm">{errors.email}</p>
+          )}
           <input
             type="password"
             value={password}
@@ -54,6 +96,9 @@ const Signup: React.FC = () => {
             placeholder="Password"
             className="mb-4 w-full p-3 border border-gray-300 rounded-md"
           />
+          {errors.password && (
+            <p className="text-red-500 text-sm">{errors.password}</p>
+          )}
           <button
             type="submit"
             className="w-full p-3 bg-black text-white rounded-md mb-4"
