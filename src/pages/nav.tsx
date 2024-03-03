173d3af2ce8d92a2904bd0ec06345439bbb3b678
Robert Stoica <robe3750@stud.kea.dk>
2024-03-03 23:25:13 +0100

    Add logo to navigation bar

--- src/pages/nav.tsx
+++ src/pages/nav.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Image from "next/image";
 import { useRouter } from "next/router";
 
 interface NavbarProps {
@@ -25,35 +26,44 @@ const Navbar: React.FC<NavbarProps> = ({
   };
 
   return (
-    <nav className="flex justify-center space-x-4 p-4">
-      {sections.map((section) => (
-        // Use onMouseEnter and onMouseLeave on the parent div
-        <div
-          key={section}
-          className="flex items-center"
-          onMouseEnter={() => setHoveredSection(section)}
-          onMouseLeave={() => setHoveredSection(null)}
-        >
+    // <nav className="flex justify-center space-x-4 p-4">
+    <nav
+      className="relative flex items-center justify-center p-4"
+      style={{ minHeight: "64px" }}
+    >
+      <div className="absolute left-0 pl-4">
+        <Image src="/icons/logo.svg" alt="Logo" width={100} height={50} />
+      </div>
+      <div className="flex justify-center space-x-4">
+        {sections.map((section) => (
+          // Use onMouseEnter and onMouseLeave on the parent div
           <div
-            className={`h-4 w-4 rounded-full  cursor-pointer transition duration-800 ease-in-out ${
-              activeSection === section
-                ? "bg-black"
-                : hoveredSection === section
-                ? "bg-gray-500"
-                : "bg-transparent border-2 border-black"
-            }`}
-            // onClick={() => setActiveSection(section)}
-            onClick={() => handleSectionClick(section)}
-          />
-          <button
-            className="text-black py-2 px-4"
-            // onClick={() => setActiveSection(section)}
-            onClick={() => handleSectionClick(section)}
+            key={section}
+            className="flex items-center"
+            onMouseEnter={() => setHoveredSection(section)}
+            onMouseLeave={() => setHoveredSection(null)}
           >
-            {section}
-          </button>
-        </div>
-      ))}
+            <div
+              className={`h-4 w-4 rounded-full  cursor-pointer transition duration-800 ease-in-out ${
+                activeSection === section
+                  ? "bg-black"
+                  : hoveredSection === section
+                  ? "bg-gray-500"
+                  : "bg-transparent border-2 border-black"
+              }`}
+              // onClick={() => setActiveSection(section)}
+              onClick={() => handleSectionClick(section)}
+            />
+            <button
+              className="text-black py-2 px-4"
+              // onClick={() => setActiveSection(section)}
+              onClick={() => handleSectionClick(section)}
+            >
+              {section}
+            </button>
+          </div>
+        ))}
+      </div>
     </nav>
   );
 };
